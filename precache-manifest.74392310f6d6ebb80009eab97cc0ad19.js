8dc1f44cad19fc03a544e3382e0326af64171861
leehe <lihechn@outlook.com>
2020-04-17 15:54:44 +0800

    deploy

---
+++ precache-manifest.74392310f6d6ebb80009eab97cc0ad19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a49496fbc5ebba7201",
+    "url": "/pocket-website/css/app.8765ddd9.css"
+  },
+  {
+    "revision": "f6f519a17b9c38c9931d8f49ef1a0728",
+    "url": "/pocket-website/index.html"
+  },
+  {
+    "revision": "80a49496fbc5ebba7201",
+    "url": "/pocket-website/js/app.ebb3ab8f.js"
+  },
+  {
+    "revision": "9c797a6ea1c97277b3eb",
+    "url": "/pocket-website/js/chunk-vendors.369be1ad.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/pocket-website/manifest.json"
+  },
+  {
+    "revision": "a715fa011abb5245b1508b1ddb05c3fa",
+    "url": "/pocket-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pocket-website/robots.txt"
+  }
+]);
